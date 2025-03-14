4bcd63f25c252d5bb9afa9978b22eb4a7edeef1d
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-03-14 17:03:26 +0200

    abc

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react';
 
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
-  const [] = useState(0);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  useEffect(() => {});
 
-  return <></>;
+  return (
+    <>
+      <SearchBar onSubmit={inputValue} />
+    </>
+  );
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,39 @@
 import css from './SearchBar.module.css';
+import { useState } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import { useId } from 'react';
+import { FcSearch } from 'react-icons/fc';
 
 const SearchBar = ({ onSubmit }) => {
-  const id = useId();
-  const handleClear = () => {
-    onFilter('');
-  };
+  const [image, setImage] = useState('');
 
+  const handleChange = evt => {
+    setImage(evt.target.value);
+  };
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    if (!image.trim()) {
+      return alert('Empty');
+    }
+    onSubmit(image);
+    setImage('');
+  };
   return (
     <div className={css.container}>
-      <header>
-        <form>
+      <header className={css.header}>
+        <form className={css.form} onSubmit={handleSubmit}>
           <input
+            onChange={handleChange}
+            name="search"
+            value={image}
+            className={css.input}
             type="text"
             autocomplete="off"
             autofocus
             placeholder="Search images and photos"
           />
-          <button type="submit">Search</button>
+          <button className={css.btn} type="submit">
+            <FcSearch size="40" />
+          </button>
         </form>
       </header>
     </div>
